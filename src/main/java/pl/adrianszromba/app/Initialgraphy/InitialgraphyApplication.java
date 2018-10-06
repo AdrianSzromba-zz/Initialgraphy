aab471a90d5d804f0f4e8fe7bac44c6ee4440ac1
AdrianSzromba <adrian.szromba@gmail.com>
2018-10-06 11:30:31 +0200

    Initial commit

---
+++ src/main/java/pl/adrianszromba/app/Initialgraphy/InitialgraphyApplication.java
@@ -0,0 +1,12 @@
+package pl.adrianszromba.app.Initialgraphy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitialgraphyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitialgraphyApplication.class, args);
+	}
+}
