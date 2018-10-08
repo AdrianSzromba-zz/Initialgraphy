ea7552f3360ee3441c32f78a1cedbde529e253e8
AdrianSzromba <adrian.szromba@gmail.com>
2018-10-08 22:42:32 +0200

    add service layer with UserService interface and implementation of it

---
+++ src/main/java/pl/adrianszromba/app/Initialgraphy/service/UserService.java
@@ -0,0 +1,7 @@
+package pl.adrianszromba.app.Initialgraphy.service;
+
+import pl.adrianszromba.app.Initialgraphy.appl.dto.UserDto;
+
+public interface UserService {
+    UserDto createUser (UserDto user);
+}
---
+++ src/main/java/pl/adrianszromba/app/Initialgraphy/service/impl/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package pl.adrianszromba.app.Initialgraphy.service.impl;
+
+import pl.adrianszromba.app.Initialgraphy.appl.dto.UserDto;
+import pl.adrianszromba.app.Initialgraphy.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public UserDto createUser(UserDto user) {
+        return null;
+    }
+}
