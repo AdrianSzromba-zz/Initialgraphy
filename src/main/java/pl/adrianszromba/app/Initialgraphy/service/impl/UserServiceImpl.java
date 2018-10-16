32ecac9bf66f513efac409194bcb2b76d5eae746
AdrianSzromba <adrian.szromba@gmail.com>
2018-10-16 23:05:21 +0200

    implement user id generator

--- src/main/java/pl/adrianszromba/app/Initialgraphy/service/impl/UserServiceImpl.java
+++ src/main/java/pl/adrianszromba/app/Initialgraphy/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.adrianszromba.app.Initialgraphy.appl.dto.UserDto;
 import pl.adrianszromba.app.Initialgraphy.appl.repository.UserRepository;
+import pl.adrianszromba.app.Initialgraphy.appl.utils.Utils;
 import pl.adrianszromba.app.Initialgraphy.io.entity.UserEntity;
 import pl.adrianszromba.app.Initialgraphy.service.UserService;
 
@@ -12,10 +13,12 @@ import pl.adrianszromba.app.Initialgraphy.service.UserService;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final Utils utils;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, Utils utils) {
         this.userRepository = userRepository;
+        this.utils = utils;
     }
 
     @Override
@@ -27,8 +30,9 @@ public class UserServiceImpl implements UserService {
         UserEntity userEntity = new UserEntity();
         BeanUtils.copyProperties(user, userEntity);
 
+        String publicUserId = utils.generateUserId(30);
         userEntity.setEncryptedPassword("test");
-        userEntity.setUserId("testUserId");
+        userEntity.setUserId(publicUserId);
 
         UserEntity storedUserDetails = userRepository.save(userEntity);
 
