8b72cdbb3325de4b180cb3b790ba7049351e6746
AdrianSzromba <adrian.szromba@gmail.com>
2018-10-22 21:03:50 +0200

    implementation of security layer

--- src/main/java/pl/adrianszromba/app/Initialgraphy/service/UserService.java
+++ src/main/java/pl/adrianszromba/app/Initialgraphy/service/UserService.java
@@ -1,7 +1,8 @@
 package pl.adrianszromba.app.Initialgraphy.service;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
 import pl.adrianszromba.app.Initialgraphy.appl.dto.UserDto;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     UserDto createUser (UserDto user);
 }
