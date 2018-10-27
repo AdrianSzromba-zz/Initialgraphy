4ea674e7b76b52ace1b5917ae7ba42f28d113890
AdrianSzromba <adrian.szromba@gmail.com>
2018-10-27 21:34:58 +0200

    change URL path for user login filter from login to users/login

--- src/main/java/pl/adrianszromba/app/Initialgraphy/security/WebSecurity.java
+++ src/main/java/pl/adrianszromba/app/Initialgraphy/security/WebSecurity.java
@@ -11,22 +11,35 @@ import pl.adrianszromba.app.Initialgraphy.service.UserService;
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
-    private final UserService userDetailsService;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+	private final UserService userDetailsService;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
 
-    public WebSecurity(UserService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
-        this.userDetailsService = userDetailsService;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-    }
+	public WebSecurity(UserService userDetailsService,
+	                   BCryptPasswordEncoder bCryptPasswordEncoder) {
+		this.userDetailsService = userDetailsService;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+	}
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll().anyRequest().authenticated();
-    }
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf()
+				.disable()
+				.authorizeRequests()
+				.antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
+				.permitAll()
+				.anyRequest()
+				.authenticated().and().addFilter(getAuthenticationFilter());
+	}
 
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
-    }
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+	}
+
+	public AuthenticationFilter getAuthenticationFilter() throws Exception {
+		final AuthenticationFilter filter = new AuthenticationFilter(authenticationManager());
+		filter.setFilterProcessesUrl("/users/login");
+		return filter;
+	}
 }
