8382814a68bd0ae69133f113562ae49086953c0e
AdrianSzromba <adrian.szromba@gmail.com>
2018-10-29 18:33:47 +0100

    Create new Filter Class for Authorization login user

---
+++ src/main/java/pl/adrianszromba/app/Initialgraphy/security/AuthorizationFilter.java
@@ -0,0 +1,34 @@
+package pl.adrianszromba.app.Initialgraphy.security;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthorizationFilter extends BasicAuthenticationFilter {
+
+	public AuthorizationFilter(AuthenticationManager authManager) {
+		super(authManager);
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+	                                FilterChain chain) throws IOException, ServletException {
+		String header = request.getHeader(SecurityConstants.HEADER_STRING);
+
+		if (header == null || !header.startsWith(SecurityConstants.TOKEN_PREFIX)) {
+			chain.doFilter(request, response);
+			return;
+		}
+
+		UsernamePasswordAuthenticationToken authentication = getAuthentication(request);
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		chain.doFilter(request, response);
+	}
+}
